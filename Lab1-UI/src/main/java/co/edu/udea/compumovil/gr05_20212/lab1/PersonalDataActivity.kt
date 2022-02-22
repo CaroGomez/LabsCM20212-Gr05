511c57e82b2ea44fc2bb79c1b8e15ee262392569
Jhon Ballesteros <jhon.ballesteros@udea.edu.co>
2022-02-22 12:07:43 -0500

    Datepicker for BirthDay

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20212/lab1/PersonalDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20212/lab1/PersonalDataActivity.kt
@@ -1,9 +1,17 @@
 package co.edu.udea.compumovil.gr05_20212.lab1
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PersonalDataActivity : AppCompatActivity() {
+    var cal = Calendar.getInstance()
+    var txtDate: TextView?=null
+    var btnDate : Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personal_data)
@@ -11,5 +19,50 @@ class PersonalDataActivity : AppCompatActivity() {
 
         actionBar!!.title = "Personal Data"
         actionBar.setDisplayHomeAsUpEnabled(true)
+
+        val datePicker = findViewById<DatePicker>(R.id.dtpBirthDay)
+        val today = Calendar.getInstance()
+        datePicker.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)){ view, year, month, day ->
+                val month = month + 1
+                val msg = "You Selected: $day/$month/$year"
+                Toast.makeText(this@PersonalDataActivity, msg, Toast.LENGTH_SHORT).show()
+            }
+/*        // get the references from layout file
+         txtDate = findViewById<TextView>(R.id.txtViewDate)
+         btnDate = findViewById<Button>(R.id.btnDate)
+
+        txtDate!!.text = "--/--/----"
+
+        // create an OnDateSetListener
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
+        btnDate!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@PersonalDataActivity,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+
+
+            }
+
+        })*/
+    }
+    private fun updateDateInView() {
+        val myFormat = "MM/dd/yyyy" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        txtDate!!.text = sdf.format(cal.getTime())
     }
 }
