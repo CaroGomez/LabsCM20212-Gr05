f85d6f4f90869869dbe6a193ead8d3abbcaa5949
Miguel Angel Urueña Riobo <cayu2312002@gmail.com>
2022-02-24 00:03:45 -0500

    Se añadió autocompletado con paises de Latam y ciudades principales de Colombia. Corrección de errores menores.

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20212/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20212/lab1/ContactDataActivity.kt
@@ -2,10 +2,30 @@ package co.edu.udea.compumovil.gr05_20212.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import kotlinx.android.synthetic.main.activity_contact_data.*
 
 class ContactDataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_data)
+
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "Contact Data"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+
+
+        val countries = resources.getStringArray(R.array.countries_array);
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, countries)
+        actv_country.setAdapter(adapter)
+
+        val cities = resources.getStringArray(R.array.cities_array);
+        val adapter2 = ArrayAdapter(this, android.R.layout.simple_list_item_1, cities)
+        actv_City.setAdapter(adapter2)
     }
+
 }
